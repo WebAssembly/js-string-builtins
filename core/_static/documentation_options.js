262db534dce3ab23a4ab2d4ad06ef519e78e181f
eqrion <eqrion@users.noreply.github.com>
2024-08-08 18:04:51 +0000

    deploy: 8f2e8dcf40c45a1d7f2e7a78202bb233904ea2f7

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0 + js-string-builtins (Draft 2024-08-06)',
+    VERSION: '3.0 + js-string-builtins (Draft 2024-08-08)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
